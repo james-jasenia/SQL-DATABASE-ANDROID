2ca33a473b7490683f369406d69bbb121044959b
James <james.jasenia@gmail.com>
2021-01-04 15:02:52 +1100

    Initial commit

---
+++ app/src/main/java/com/example/sqldatabaseprototype/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sqldatabaseprototype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('James', 30)");
+            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Sarah', 28)");
+
+            //Cursor c is convention
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+
+            while (!c.isAfterLast()) {
+                Log.i("Name", c.getString(nameIndex));
+                Log.i("Age", c.getString(ageIndex));
+                c.moveToNext();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
